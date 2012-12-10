16293cae47b46ca1162d35bdab7202d5a89f2d9c
Djun Kim <Djun.Kim@cielo>
2012-12-10 10:26:39 -0800

    Improved connection handling and added support for dynamically generated R-plots

--- macros/rserve.pl
+++ macros/rserve.pl
@@ -1,3 +1,27 @@
+=head1 NAME
+
+rserve.pl - Macros for querying an Rserve server (see R-project.org)
+
+=head1 SYNPOSIS
+
+Example: generate a normally distributed vector of 15 elements,
+with mean 1, standard deviation 2, rounded to 4 decimal places.
+
+ $m = 1;
+ $sd = 2;
+ @rnorm = rserve_query(EV2(<<END_RCODE));
+   data1=rnorm(15,mean=$m,sd=$sd)
+   round(data1,4)
+ END_RCODE
+
+=head1 DESCRIPTION
+
+The macros in this file set up a connection to the R server and
+pass a string parameter to R for evaluation.  The resulting
+vector is returned as a perl array object.
+
+=cut
+
 use Rserve::Connection;
 #use strict;
 #use warnings;
@@ -5,33 +29,88 @@ use Rserve::Connection;
 # Rserve connection
 my $cnx;
 
-sub _rserve_init {};
+sub _rserve_init {
+};
 
-#sub rserve_start {
-#   $cnx = Rserve::Connection->new('localhost');
-#}
+sub rserve_start {
+    if (!defined $cnx or ref($cnx) != "Rserve::Connection") {
+	$cnx = Rserve::Connection->new('localhost');
+    }
 
-#sub rserve_finish {
-#    if (ref($cnx)=="Rserve::Connection") {
-#	$cnx->close();
-#    }
-#}
+    # Ensure R's random number generation is given a well-defined seed.
+    # $problemSeed is the environmental variable defined by WeBWorK which
+    # gives the random seed associated to a given problem/user assignment.
 
-#sub rserve_eval { 
-#  my $query = shift; 
-#  my @res = $cnx->evalString($query);
-#  return @res;
-#}
+    my $query = "set.seed($problemSeed)\n";
+    $cnx->evalString($query);
+}
 
-#sub rserve_query {
-#  my $query = shift; 
-#  my $rserve_client = Rserve::Connection->new('localhost');
-#  my @res = $rserve_client->evalString($query);
-#  #print ("result = $res");
-#  return @res;
-#}
+sub rserve_finish {
+    if (ref($cnx)=="Rserve::Connection") {
+	$cnx->close();
+    }
+}
 
-#sub rserve_plot_png {
-#}
+sub rserve_eval { 
+  my $query = shift; 
+
+  if (ref($cnx) != "Rserve::Connection") {
+      $cnx = Rserve::Connection->new('localhost');
+  }
+  my @res = $cnx->evalString($query);
+  return @res;
+}
+
+
+sub rserve_query {
+  my $query = shift; 
+  $query = "set.seed($problemSeed)\n" . $query;
+  my $rserve_client = Rserve::Connection->new('localhost');
+  my @res = $rserve_client->evalString($query);
+  #print ("result = $res");
+  return @res;
+}
+
+sub rserve_start_plot ($) {
+    my $imgtype = shift;
+    my $rand = sprintf("%05d", random(0, 99999, 1));
+
+    my $filename = "tmpfile".$rand;
+
+    if ($imgtype == 'png') {
+	$suffix = ".png";
+	$filename .=  $suffix;
+	rserve_eval("png(filename='/tmp/$filename')");
+    }
+    elsif ($imgtype = 'jpg') {
+	$suffix = ".jpg";
+	$filename .=  $suffix;
+	rserve_eval("jpeg(filename='/tmp/$filename')");
+    }
+    elsif ($imgtype = 'pdf') {
+	$suffix = ".pdf";
+	$filename .=  $suffix;
+	rserve_eval("pdf(filename='/tmp/$filename')");
+    }
+    else {
+	warn "unknown/unsupported image type '$imgtype'\n";
+    }
+
+    return $filename;
+}
+
+sub rserve_finish_plot ($) {
+    my $file = shift;
+    my $filename = $tempDirectory . "/$file";
+
+    rserve_eval("dev.off()");
+    @stream = rserve_eval("readBin('/tmp/$file', what='raw', n=1e6)");
+
+    open BINARY, ">:raw", $filename;
+    foreach (@stream) { print BINARY $_}
+    close BINARY;
+    
+    return $filename;
+}
 
 1;
